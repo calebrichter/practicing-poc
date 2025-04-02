213d0d2ff70b3cf571ab6105029f3a74c5f18066
Caleb Richter <calebrichter.crd@gmail.com>
2025-04-01 22:08:16 -0400

    Updated schema with churches, sermons, connections

--- practicing-poc/schema.ts
+++ practicing-poc/schema.ts
@@ -27,6 +27,7 @@ import { document } from '@keystone-6/fields-document'
 import { type Lists } from '.keystone/types'
 
 export const lists = {
+
   User: list({
 
     access: allowAll,
@@ -39,8 +40,6 @@ export const lists = {
 
       email: text({
         validation: { isRequired: true },
-        // by adding isIndexed: 'unique', we're saying that no user can have the same
-        // email as another user - this may or may not be a good idea for your project
         isIndexed: 'unique',
       }),
 
@@ -48,8 +47,8 @@ export const lists = {
 
       // we can use this field to see what Posts this User has authored
       //   more on that in the Post list below
-      posts: relationship({ ref: 'Post.author', many: true }),
-
+      sermons: relationship({ ref: 'Sermon.speaker', many: true }),
+      church: relationship({ ref: 'Church', many: false }),
       createdAt: timestamp({
         // this sets the timestamp to Date.now() when the user is first created
         defaultValue: { kind: 'now' },
@@ -57,90 +56,38 @@ export const lists = {
     },
   }),
 
-  Post: list({
-    // WARNING
-    //   for this starter project, anyone can create, query, update and delete anything
-    //   if you want to prevent random people on the internet from accessing your data,
-    //   you can find out more at https://keystonejs.com/docs/guides/auth-and-access-control
+  Church: list({
     access: allowAll,
-
-    // this is the fields for our Post list
     fields: {
-      title: text({ validation: { isRequired: true } }),
-
-      // the document field can be used for making rich editable content
-      //   you can find out more at https://keystonejs.com/docs/guides/document-fields
-      content: document({
-        formatting: true,
-        layouts: [
-          [1, 1],
-          [1, 1, 1],
-          [2, 1],
-          [1, 2],
-          [1, 2, 1],
-        ],
-        links: true,
-        dividers: true,
-      }),
-
-      // with this field, you can set a User as the author for a Post
-      author: relationship({
-        // we could have used 'User', but then the relationship would only be 1-way
-        ref: 'User.posts',
-
-        // this is some customisations for changing how this will look in the AdminUI
-        ui: {
-          displayMode: 'cards',
-          cardFields: ['name', 'email'],
-          inlineEdit: { fields: ['name', 'email'] },
-          linkToItem: true,
-          inlineConnect: true,
-        },
-
-        // a Post can only have one author
-        //   this is the default, but we show it here for verbosity
-        many: false,
-      }),
-
-      // with this field, you can add some Tags to Posts
-      tags: relationship({
-        // we could have used 'Tag', but then the relationship would only be 1-way
-        ref: 'Tag.posts',
-
-        // a Post can have many Tags, not just one
-        many: true,
-
-        // this is some customisations for changing how this will look in the AdminUI
-        ui: {
-          displayMode: 'cards',
-          cardFields: ['name'],
-          inlineEdit: { fields: ['name'] },
-          linkToItem: true,
-          inlineConnect: true,
-          inlineCreate: { fields: ['name'] },
-        },
-      }),
+      name: text({ validation: { isRequired: true } }),
+      city: text({ validation: { isRequired: true } }),
+      state: text({ validation: { isRequired: true } }),
+      sermons: relationship({ ref: 'Sermon.church', many: true }),
+      website: text({ validation: { isRequired: false },}),
     },
   }),
 
-  // this last list is our Tag list, it only has a name field for now
-  Tag: list({
-    // WARNING
-    //   for this starter project, anyone can create, query, update and delete anything
-    //   if you want to prevent random people on the internet from accessing your data,
-    //   you can find out more at https://keystonejs.com/docs/guides/auth-and-access-control
+  Sermon: list({
     access: allowAll,
-
-    // setting this to isHidden for the user interface prevents this list being visible in the Admin UI
-    ui: {
-      isHidden: true,
-    },
-
-    // this is the fields for our Tag list
     fields: {
-      name: text(),
-      // this can be helpful to find out all the Posts associated with a Tag
-      posts: relationship({ ref: 'Post.tags', many: true }),
+      title: text({ validation: { isRequired: true } }),
+      date: timestamp({ validation: { isRequired: true } }),
+      church: relationship({ ref: 'Church.sermons', many: false }),
+      speaker: relationship({ ref: 'User.sermons', many: false }),
+      transcript: document(
+        {
+          formatting: true,
+          links: true,
+        },
+      ),
+      outline: document(
+        {
+          formatting: true,
+          links: true,
+        },
+      ),
+      summary: text(),
+      link: text(),
     },
   }),
 } satisfies Lists
